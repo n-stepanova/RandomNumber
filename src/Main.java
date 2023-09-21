370620292ce700383e12db3617590e72048adddd
n-stepanova <nstepanovaqa@gmail.com>
2023-09-21 20:23:16 +0300

    randomizer

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int generateRandomValue(){
+        int value = (int) (1 + Math.random()*100);
+        return value;
+    }
+
+    public static int getUserValue(){
+        int userValue = 0;
+        boolean isInteger = false;
+        while (isInteger==false) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Введите ваше число: ");
+            if (scanner.hasNextInt()) {
+                isInteger=true;
+                userValue = scanner.nextInt();
+            }
+            else{
+                System.out.println("Ошибка");
+            }
+        }
+        return userValue;
+    }
+
+    public static void playTheGame(int randomizerValue){
+        System.out.println("Рандомайзер загадал число, попробуйте угадать))");
+
+        boolean userIsWin = false;
+        while (userIsWin==false){
+            int userNum = getUserValue();
+            if (userNum>randomizerValue){
+                System.out.println("Ваше число больше загаданного");
+            }else if (userNum<randomizerValue){
+                System.out.println("Ваше число меньше загаданного");
+            }
+            else if (userNum==randomizerValue){
+                userIsWin=true;
+            }
+        }
+        System.out.println("Поздравляем, вы угадали число: " + randomizerValue);
+    }
+
+    public static void main(String[] args) {
+        int randomValue = generateRandomValue();
+        playTheGame(randomValue);
+    }
+}
