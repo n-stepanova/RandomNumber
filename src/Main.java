2d8504906c0e5c410023ef85ddca24957020d461
n-stepanova <nstepanovaqa@gmail.com>
2023-09-21 20:55:57 +0300

    add help info

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 
 public class Main {
     public static final int MAX_LIMIT = 100;
-    public static final int MIN_INT = 1;
+    public static final int MIN_LIMIT = 1;
+
+    public static int maxBorder;
+    static int minBorder;
+
     public static int generateRandomValue(){
-        int value = (int) (1 + Math.random()*100);
+        int value = (int) (MIN_LIMIT + Math.random()*MAX_LIMIT);
         return value;
     }
 
@@ -26,15 +30,20 @@ public class Main {
     }
 
     public static void playTheGame(int randomizerValue){
+        maxBorder = MAX_LIMIT;
+        minBorder = MIN_LIMIT;
         System.out.println("Рандомайзер загадал число, попробуйте угадать))");
 
         boolean userIsWin = false;
         while (userIsWin==false){
+            System.out.println("\n*** Ищите в диапазоне от " + minBorder + " до " + maxBorder + " ***");
             int userNum = getUserValue();
             if (userNum>randomizerValue){
-                System.out.println("Ваше число больше загаданного");
+                System.out.println("Ваше число больше загаданного. ");
+                maxBorder = userNum;
             }else if (userNum<randomizerValue){
                 System.out.println("Ваше число меньше загаданного");
+                minBorder = userNum;
             }
             else if (userNum==randomizerValue){
                 userIsWin=true;
